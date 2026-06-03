128619b65dc3fa838c161f3ab406a4b99f09fb35
Minecraft0122 <168195378+Minecraft0122@users.noreply.github.com>
2026-06-03 23:08:22 +0800

    Create RewardCommand.java

---
+++ src/main/java/Simpmc/Rewards/command/RewardCommand.java
@@ -0,0 +1,27 @@
+package Simpmc.Rewards.command;
+
+import Simpmc.Rewards.gui.MainMenu;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class RewardCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(
+            CommandSender sender,
+            Command command,
+            String label,
+            String[] args
+    ) {
+
+        if (!(sender instanceof Player player)) {
+            return true;
+        }
+
+        MainMenu.open(player);
+
+        return true;
+    }
+}
